7137db79769216e68e10b52732fd6cd64a7beee2
Vicente <vicente.yah.dzul@icloud.com>
2020-04-16 23:04:09 -0500

    error with data types corrected at the end point

--- app/models/JobExchanges.js
+++ app/models/JobExchanges.js
@@ -1,29 +1,25 @@
 module.exports = (sequelize, Sequelize) => {
     const JobExchange = sequelize.define("jobexchanges", {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true
-      },
       name: {
         type: Sequelize.STRING
       },
       age:{
-          type: Sequelize.INTEGER
+        type: Sequelize.INTEGER
       },
       gender:{
-          type: Sequelize.STRING
+        type: Sequelize.STRING
       },
       civilstatus: {
-          type: Sequelize.STRING
+        type: Sequelize.STRING
       },
       phone: {
-          type: Sequelize.STRING
+        type: Sequelize.STRING
       },
       occupation: {
-          type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       jobname: {
-          type: Sequelize.STRING
+        type: Sequelize.STRING
       }
 
     });
