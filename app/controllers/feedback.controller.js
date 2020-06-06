78d21cdae400ee37552a7f59fbdcaf04de098f4b
Vicente <vicente.yah.dzul@icloud.com>
2020-06-06 06:29:56 -0500

    update method for feedbacks were created

--- app/controllers/feedback.controller.js
+++ app/controllers/feedback.controller.js
@@ -49,3 +49,22 @@ exports.deleteFeedbackRequest = (req,res) => {
         res.status(404).send({message:'item not found'})
     })
 }
+exports.updateFeedbackRequest = (req,res) => {
+    Feedback.update({
+        completename:req.body.completename,
+        email: req.body.email,
+        phone: req.body.phone,
+        store: req.body.store,
+        message: req.body.message
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }).then(feedbackUpdate => {
+        res.status(200).send({message:'rows updated successfully'})
+        console.log(feedbackUpdate);
+    }).catch(error =>  {
+        res.status(404).send({message:'item not found'})
+    })
+}
