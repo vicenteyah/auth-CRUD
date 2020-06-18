625adf5e6d1e1476b252c0e9b1f3f867181df7a4
SariMelina <mmkings_beba@hotmail.com>
2020-06-18 01:40:19 -0500

    New route imported

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ require('./app/routes/Purchases.routes')(app)
 require('./app/routes/Pizza.routes')(app);
 require('./app/routes/Additional.routes')(app);
 require('./app/routes/Extra.routes')(app);
+require('./app/routes/Store.routes')(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
